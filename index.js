1662d8c9efde27b2659702510d941400f5eefcbb
nanut <galunyu@gmail.com>
2019-10-18 11:48:14 +0700

    ignore node_modules

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const momet = require('moment')
+const members = require('./Members')
 
 const app = express()
 
@@ -7,36 +9,20 @@ const app = express()
 // res.send('<h1>Hello ExpressJs</h1>')
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // })
-const members = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-    status: "active"
-  },
-  {
-    id: 2,
-    name: "Bob Williams",
-    email: "bob@gmail.com",
-    status: "inactive"
-  },
-  {
-    id: 3,
-    name: "Shannon jackson",
-    email: "shannon@gmail.com",
-    status: "active"
-  },
-]
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.git('host')}${res.originalUrl}`)
+  next()
+}
 
+//? init middleware
+app.use(logger)
 
-app.get('/api/members', (req, res) => {
+//? Get All members
+app.get('/api/members', (req, res) => res.json(members))
 
+// //? Set static folder
+// app.use(express.static(path.join(__dirname, 'public')))
 
-})
-
-
-//? Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
